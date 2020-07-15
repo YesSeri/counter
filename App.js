667cc74205d863a4bb51dddfaf99cd79037f7f5b
YesSeri <henrik.zenkert@gmail.com>
2020-07-15 23:12:51 +0200

    Counter is complete Build it on laptop

--- App.js
+++ App.js
@@ -1,38 +1,102 @@
 import React from 'react';
-import { View, Button, Text, ScrollView, StyleSheet, Switch } from 'react-native';
+import { View, TextInput, Text, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
+    paddingTop: Constants.statusBarHeight,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  count: {
-    fontSize: 48
-  }
+  textContainer: {
+    borderRadius: 20,
+    width: '80%',
+    height: '40%',
+    justifyContent: 'center'
+  },
+  textStyle: {
+    fontSize: 100,
+    textAlign: 'center',
+  },
+  textInputContainer: {
+    paddingTop: 10,
+  },
 })
+let primaryBg = '#eee'
+let secondaryBg = '#f8f8f8'
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      count: 0,
+      count: 1,
+      bgColors: [primaryBg, secondaryBg],
+      bgColor: primaryBg,
+      textBgColor: secondaryBg,
+      colors: ['#111', '#087f23', '#4caf50', '#ff8a50', '#ff5722'],
+      color: '#111',
+      changeTime: 6,
     };
   }
-  
-  componentDidMount(){
-    setInterval(this.inc(), 1000)
-  }
 
+  componentDidMount(){
+    setInterval(this.inc, 1000)
+  }
+  changeBgColor(){
+    this.setState(prevState => ({
+      bgColor: prevState.textBgColor,
+      textBgColor: prevState.bgColor,
+    }))
+  }
+  calculateIndex(){
+    if (this.state.count % this.state.changeTime === 0) {
+      const index = this.state.colors.indexOf(this.state.color)
+      return index >= this.state.colors.length - 1 ? 0 : index + 1;
+    } else {
+      return this.state.colors.indexOf(this.state.color)
+    }
+  }
   inc = () => {
+    if ((this.state.count) % (this.state.changeTime*this.state.colors.length) === 0) {
+      this.changeBgColor()
+    }
+    let newIndex = this.calculateIndex();
     this.setState(prevState => ({
       count: prevState.count + 1,
+      color: prevState.colors[newIndex],
     }))
   }
 
+  onChanged = (text) => {
+    const number = parseInt(text)
+    this.setState({
+      changeTime: number,
+    })
+  }
+  reset(){
+    this.setState({
+      count: 1,
+    })
+  }
   render() {
     return (
-      <View style={styles.appContainer}>
-        <Text style={styles.count}>{this.state.count}</Text>
+      <View style={[styles.appContainer, { backgroundColor: this.state.bgColor}]}>
+        <View 
+          style={[styles.textContainer, { backgroundColor: this.state.textBgColor}]}
+          onTouchStart={() => this.reset()}
+        >
+          <Text style={[styles.textStyle, { color: this.state.color}]}>
+              {this.state.count - 1}
+          </Text>
+        </View>
+        <View>
+          <TextInput 
+            style={styles.textInputContainer}
+            textAlign='center'
+            placeholder="Number"  
+            onChangeText={text => this.onChanged(text)}
+            keyboardType='number-pad'
+          />
+        </View>
       </View>
     );
   }
